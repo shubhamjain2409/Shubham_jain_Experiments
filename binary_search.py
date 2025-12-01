fbf0961814963cd2b20313365c68bb168116f513
shubhamjain2409 <shubhamjain2409@gmail.com>
2025-12-01 15:41:49 +0530

    Adding Binary Search Code

---
+++ binary_search.py
@@ -0,0 +1,20 @@
+def binary_search(arr,target):
+
+    l = 0
+    r = len(arr) - 1
+
+    while(l<=r):
+        mid = l + (r-l)//2
+
+        if(arr[mid] == target):
+            return mid
+        elif(arr[mid] < target):
+            l = mid + 1
+
+        else:
+            r = mid - 1
+
+    return -1
+
+
+        
